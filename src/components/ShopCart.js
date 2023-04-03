9ccf528f242dee73fee3d7d38ee1d6dec7a0d77f
VitorAlecrim <vitor.alecrim.p@gmail.com>
2023-04-03 15:32:42 -0300

    1.7.2

--- src/components/ShopCart.js
+++ src/components/ShopCart.js
@@ -18,8 +18,26 @@ const ShopCart = ()=>{
                         <h2>CARRINHO</h2>
                         <button onClick={()=>{dispatch(toggleCart(false))}}  className="close-button" >X</button>
                     </div>
-                    <div className="content">
-                        <h3 className="empty-message" >carrinho vazio</h3>
+                    <div className={`content ${listOfItems.length === 0 && 'empty-cart'}  `}>
+                        {listOfItems.length === 0 && (<h3 className="empty-message" >carrinho vazio</h3>)}
+                        {listOfItems.length > 0 &&(
+                            <div className='table'>
+                                <div className='image'>
+                                    <img src={listOfItems[0].productImage} />
+                                </div>
+                                <div className='body'>
+                                    {/* <div className='head' > */}
+                                        <h3 className='product-name' >{listOfItems[0].productName}</h3>
+                                    {/* </div> */}
+                                    <div className='product-description' >
+                                        <span>quant: {listOfItems[0].quantity} </span>
+                                        <div className='price-box'>
+                                            <span className='price' > $ {listOfItems[0].discount}</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        )}
                     </div>
                 </div>
             </aside>
